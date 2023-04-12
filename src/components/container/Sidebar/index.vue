82addcfd90f93143b0d17199438406d3b44f45ab
un-hum <383418809@qq.com>
2023-04-12 23:01:21 +0800

    feat: 侧边栏选中逻辑优化

--- src/components/container/Sidebar/index.vue
+++ src/components/container/Sidebar/index.vue
@@ -2,17 +2,17 @@
  * @Author: un-hum 383418809@qq.com
  * @Date: 2023-02-25 17:40:47
  * @LastEditors: un-hum 383418809@qq.com
- * @LastEditTime: 2023-04-05 15:56:54
+ * @LastEditTime: 2023-04-12 22:59:01
  * @FilePath: /nosgram/src/components/container/Sidebar/Index.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
 <template>
+  <!-- @select="_handleSelect" -->
   <el-menu
     v-show="!hide"
     :collapse="!collapse"
     :default-active="active"
     class="el-menu-vertical-demo"
-    @select="_handleSelect"
   >
     <el-menu-item index="1" @click="_to({ name: 'home' })">
       <el-icon size="18"><icon-ion-home-outline /></el-icon>
@@ -44,7 +44,7 @@ import { Vue } from "vue-class-component";
 
 export default class Sidebar extends Vue {
   screenWidth: number | string = window.innerWidth;
-  active = "1";
+  // active = "1";
   // 若使用component is=xxx的方式循环icon，会不显示svg，因为icon是动态导入的。解析出来会是直接的组件，等待后期官方解决
   // menuItem: MenuItem[] = [
   //   { name: "首页", icon: "icon-ion-home-outline" },
@@ -57,8 +57,16 @@ export default class Sidebar extends Vue {
   get collapse() {
     return this.screenWidth > 1170;
   }
-
+  get active() {
+    return { home: "1", follow: "2", setting: "3" }[
+      this.$route.name as string
+    ] as string;
+  }
   mounted() {
+    console.log("this.$route", this.$route);
+    // this.active = { home: "1", follow: "2", setting: "3" }[
+    //   this.$route.name as string
+    // ] as string;
     window.addEventListener("resize", this._getScreenWidth);
   }
   destroyed() {
@@ -69,9 +77,9 @@ export default class Sidebar extends Vue {
     this.$router.replace(path);
   }
 
-  _handleSelect(index: string) {
-    this.active = index;
-  }
+  // _handleSelect(index: string) {
+  //   this.active = index;
+  // }
 
   _getScreenWidth() {
     this.screenWidth = window.innerWidth;
